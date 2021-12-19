e6299fbf30d5f36d45282cd21d371f50fffdf6c9
yoad <yoadlanger@gmail.com>
2021-12-19 23:23:35 +0200

    - updated readme file - added Name tag for EIPs

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # terraform-aws-vpc
 terraform aws vpc module for opsschool assignments
+
+ Module creates:
+ - 1 VPC
+ - 2 private subnets
+ - 2 public subnets
+ - 1 IGW
+ - 2 NAT GWs
+ - 2 EIPs (for NATs)
+ - 3 route tables (1 for public SN, 2 for private SNs)
--- main.tf
+++ main.tf
@@ -79,6 +79,9 @@ resource "aws_subnet" "private_sn" {
 resource "aws_eip" "nat_eip" {
   count = length(var.private_subnet_cidrs)
   vpc   = true
+  tags = {
+    Name = format("%s-eip-${count.index}", var.global_name_prefix)
+  }
 }
 
 ## creating NAT GWs for the private subnets
@@ -88,7 +91,7 @@ resource "aws_nat_gateway" "nat_gw" {
   subnet_id     = aws_subnet.public_sn.*.id[count.index]
 
   tags = {
-    Name = format("%s-nat_gw-${count.index}", var.global_name_prefix)
+    Name = format("%s-nat-gw-${count.index}", var.global_name_prefix)
   }
 
   # To ensure proper ordering, it is recommended to add an explicit dependency
